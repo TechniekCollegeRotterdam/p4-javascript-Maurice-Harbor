17b5cfbb074c931dce6869f37bfbcbf757122415
Maurice Harbor <9017060@student.zadkine.nl>
2022-05-20 12:09:18 +0200

    bird-up part2

--- les-2/app.js
+++ les-2/app.js
@@ -9,3 +9,25 @@ bird.addEventListener("click", function() {
     console.log("click")
 });
 
+
+
+window.addEventListener("keydown", function(e) {
+    console.log(event);
+    if(e.key == 'ArrowRight'){
+        count += 50;
+        bird.style.left = count + 'px';
+    }
+    if(e.key == 'ArrowLeft'){
+        count -= 50;
+        bird.style.left = count + 'px';
+    }
+    if(e.key == 'ArrowUp'){
+        count += 50;
+        bird.style.bottom = count + 'px';
+    }
+    if(e.key == 'ArrowDown'){
+        count += 50;
+        bird.style.top = count + 'px';
+    }
+})
+

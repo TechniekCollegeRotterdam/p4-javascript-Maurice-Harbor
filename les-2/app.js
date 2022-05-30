d145cfa5900fc5204a5e78204e70bee5208cd271
Maurice Harbor <9017060@student.zadkine.nl>
2022-05-30 14:06:43 +0200

    oefening 3 - Change the moving bird

--- les-2/app.js
+++ les-2/app.js
@@ -14,18 +14,22 @@ bird.addEventListener("click", function() {
 window.addEventListener("keydown", function(e) {
     console.log(event);
     if(e.key == 'ArrowRight'){
+        bird.src = "img/b-right.svg"
         count += 50;
         bird.style.left = count + 'px';
     }
     if(e.key == 'ArrowLeft'){
+        bird.src = "img/b-left.svg"
         count -= 50;
         bird.style.left = count + 'px';
     }
     if(e.key == 'ArrowUp'){
+        bird.src = "img/b-up.svg"
         count += 50;
         bird.style.bottom = count + 'px';
     }
     if(e.key == 'ArrowDown'){
+        bird.src = "img/b-down.svg"
         count += 50;
         bird.style.top = count + 'px';
     }
